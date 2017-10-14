34ac3b97ed55c219d8d14862e12ebda8e5c4fac0
Davi Kawasaki <davishinjik@gmail.com>
2017-10-14 05:02:11 -0300

    Adding new features in object export + names translation

--- Code/matlab/extractEcgFeatures.m
+++ Code/matlab/extractEcgFeatures.m
@@ -1,17 +1,18 @@
-function features = extractEcgFeatures(DWTsignalPeaks, fileName) 
+function features = extractEcgFeatures(DWTsignalPeaks, fileName, age, sex, medicine) 
 
-% usage: extractEcgFeatures(DWTsignalPeaks, '../data/exported/vt-200m')
+% usage: extractEcgFeatures(DWTsignalPeaks, '../data/exported/vt-200m', 89, 'M', 'Yes')
 %
 % This function extract physiological ECG features from DWT peaks.
-%
 % features{i,1} equals to R peak amplitude in mV
-% features{i,2} equals to R-R peak time delay
-% features{i,3} equals to wave arrhythmia type
-%
-
+% features{i,2} equals to R-R peak time delay in s
+% features{i,3} equals to R peak speed in mV/s
+% features{1,4} equals to patient age data
+% features{1,5} equals to patient sex data (M/F)
+% features{1,6} equals to patient medicine intake data (Yes/No)
+% features{i,7} equals to wave arrhythmia type
 % Last version
 % extractEcgFeatures.m           D. Kawasaki			17 June 2017
-% 		      Davi Kawasaki	       17 June 2017 version 1.0
+% 		      Davi Kawasaki	       14 October 2017 version 2.0
 
 features = {};
 
@@ -29,7 +30,7 @@ for i = 1:size(DWTsignalPeaks,1)
                 else 
                     peakIntervalRR = DWTsignalPeaks{i,2}(end) - DWTsignalPeaks{i,2}(1); 
                 end
-                inst = {peakAmpR peakIntervalRR DWTsignalPeaks{i,3}};
+                inst = {peakAmpR peakIntervalRR peakAmpR/peakIntervalRR age sex medicine DWTsignalPeaks{i,3}};
                 features(end+1,:) = inst;
                 
             % Other types of arrhythmia
@@ -40,7 +41,7 @@ for i = 1:size(DWTsignalPeaks,1)
                 else 
                     peakIntervalRR = DWTsignalPeaks{i,2}(end) - DWTsignalPeaks{i,2}(1); 
                 end
-                inst = {peakAmpR peakIntervalRR DWTsignalPeaks{i,3}};
+                inst = {peakAmpR peakIntervalRR peakAmpR/peakIntervalRR age sex medicine DWTsignalPeaks{i,3}};
                 features(end+1,:) = inst;
             end
         else
@@ -53,7 +54,7 @@ end
 
 featuresExtr = strcat(fileName, '.csv');
 features
-featuresTable = cell2table(features, 'VariableNames',{'Amplitude','RR','Arritmia'});
+featuresTable = cell2table(features, 'VariableNames',{'Amplitude','RR','Velocidade','Idade','Sexo','Remedios','Arritmia'});
 writetable(featuresTable, featuresExtr);
 
 end
